b03f771186add8ed54dc5ff7bb7868742d51037f
kaushalag29 <kaushalag29@gmail.com>
2020-11-15 13:49:27 +0530

    added resume link and pic

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1Qr3srYG9KxZeLB5BeNnycM6aroxjtDu-/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
